b6bb118669b4b161c43b8c32f5c442067780d449
María Segura <114308383+mariasegura20@users.noreply.github.com>
2023-04-06 16:42:55 +0200

    Delete js directory

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-list');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('responsive');
-});
