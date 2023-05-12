92b15d658a428c0fa21ad294b36e65d854669cdf
María Segura <maria.segurabolanos@alum.uca.es>
2023-05-12 10:56:56 +0200

    Primer intento de listar reservas sin API

--- Desarrollo movil/app/src/main/java/es/uca/kartinghell/ui/bookings/BookingsFragment.kt
+++ Desarrollo movil/app/src/main/java/es/uca/kartinghell/ui/bookings/BookingsFragment.kt
@@ -6,29 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import es.uca.kartinghell.databinding.FragmentBookingsBinding
-import es.uca.kartinghell.ui.location.LocationViewModel
 
 class BookingsFragment : Fragment() {
 
     private var _binding: FragmentBookingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: BookingsAdapter
+    private lateinit var viewModel: BookingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(LocationViewModel::class.java)
-
         _binding = FragmentBookingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        recyclerView = binding.recyclerView
+        adapter = BookingsAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(BookingsViewModel::class.java)
+
+        viewModel.getBookings().observe(viewLifecycleOwner) { bookings ->
+            adapter.setBookings(bookings)
+            adapter.notifyDataSetChanged()
+        }
     }
 
     override fun onDestroyView() {
--- Desarrollo movil/app/src/main/java/es/uca/kartinghell/ui/bookings/BookingsViewModel.kt
+++ Desarrollo movil/app/src/main/java/es/uca/kartinghell/ui/bookings/BookingsViewModel.kt
@@ -5,9 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BookingsViewModel : ViewModel() {
+    private val bookingsLiveData = MutableLiveData<List<Bookings>>()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Bookings Fragment"
+    init {
+        // Lista de reservas
+        val bookings = listOf(
+            Bookings("Luna", "12345678A", "9956111111", "correo@gm.es", "12/12/12", "17:30", "18:30", 10, "", "Indoor"),
+            Bookings("Luna", "12345678A", "9956111111", "correo@gm.es", "12/12/12", "17:30", "18:30", 10, "", "Indoor"),
+            Bookings("Luna", "12345678A", "9956111111", "correo@gm.es", "12/12/12", "17:30", "18:30", 10, "", "Indoor"),
+            Bookings("Luna", "12345678A", "9956111111", "correo@gm.es", "12/12/12", "17:30", "18:30", 10, "", "Indoor")
+
+        )
+        bookingsLiveData.value = bookings
     }
-    val text: LiveData<String> = _text
+
+    fun getBookings(): LiveData<List<Bookings>> {
+        return bookingsLiveData
+    }
 }
