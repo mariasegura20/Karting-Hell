6ebefdaf9c0a9f8493cdb685d3876e37b28856be
Carlos Vidal <80911346+elcarlillo@users.noreply.github.com>
2024-02-20 11:18:49 +0100

    Delete Karting Hell directory

--- Karting Hell/Desarrollo movil/app/src/main/java/es/uca/kartinghell/ui/bookings/Bookings.kt
+++
@@ -1,5 +0,0 @@
-package es.uca.kartinghell.ui.bookings
-
-data class Bookings(val name: String, val dni: String, val phone: String, val email: String, val date: String, val startHour: String, val endHour: String, val numPeople: Int, val suggestions: String?, val circuit: String)
-
-
--- Karting Hell/Desarrollo movil/app/src/main/java/es/uca/kartinghell/ui/circuits/Circuito.kt
+++
@@ -1,3 +0,0 @@
-package es.uca.kartinghell.ui.circuits
-
-data class Circuito(val Nombre: String , val Imagen: Int, val Descripcion: String, val Registro: String, val Foto: String, val Descuentos: String, val Tabla: String)
